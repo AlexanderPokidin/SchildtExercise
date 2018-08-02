c32b1e3e1948f32cd9795517f7c3891cf3aab3fe
AlexanderPokidin <a.pokidin@gmail.com>
2018-08-02 17:13:47 +0300

    CodWars

--- src/FindOdd.java
+++ src/FindOdd.java
@@ -1,12 +1,16 @@
 public class FindOdd {
     public static int findIt(int[] a) {
         int odd = 0;
+
         for (int i = 0; i < a.length; i++) {
-            for (int j = a.length - 1; j >= 0; j--) {
-                if (a[i] != a[j]) {
-                    odd = a[i];
-                    break;
+            int count = 0;
+            for (int j = 0; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    count++;
                 }
+            }
+            if (count % 2 != 0) {
+                odd = a[i];
             }
         }
         return odd;
@@ -14,13 +18,14 @@ public class FindOdd {
 
     public static void main(String[] args) {
         int res1 = findIt(new int[]{20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5});
-        int res2 = findIt(new int[]{1,1,2,-2,5,2,4,4,-1,-2,5});
-        int res3 = findIt(new int[]{20,1,1,2,2,3,3,5,5,4,20,4,5});
+        int res2 = findIt(new int[]{1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5});
+        int res3 = findIt(new int[]{20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5});
         int res4 = findIt(new int[]{10});
-        int res5;
+        int res5 = findIt(new int[]{1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1});
         System.out.println(res1);
         System.out.println(res2);
         System.out.println(res3);
         System.out.println(res4);
+        System.out.println(res5);
     }
 }
